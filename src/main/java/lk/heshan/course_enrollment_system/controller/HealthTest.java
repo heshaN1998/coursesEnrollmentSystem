73a2c8bd92e8326a3f4fcc5587d132fee7924d34
heshan <heshansandakelum2018@gmail.com>
2026-03-20 23:57:09 +0530

    building Student Controller

--- src/main/java/lk/heshan/course_enrollment_system/controller/HealthTest.java
+++ src/main/java/lk/heshan/course_enrollment_system/controller/HealthTest.java
@@ -1,4 +1,14 @@
 package lk.heshan.course_enrollment_system.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/healthtest")
 public class HealthTest {
+    @GetMapping
+    private String StudentDataTest(){
+        return "student controller is running";
+    }
 }
--- src/main/java/lk/heshan/course_enrollment_system/controller/StudentController.java
+++ src/main/java/lk/heshan/course_enrollment_system/controller/StudentController.java
@@ -1,4 +1,48 @@
 package lk.heshan.course_enrollment_system.controller;
 
+import lk.heshan.course_enrollment_system.dto.Role;
+import lk.heshan.course_enrollment_system.dto.UserDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@RequestMapping("api/v1/student")
+
 public class StudentController {
+//save Student
+
+@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+public ResponseEntity<UserDTO>saveStudent(@RequestBody UserDTO userDTO){
+    return new ResponseEntity<>(userDTO, HttpStatus.CREATED);
+
+}
+
+@GetMapping(value = "{studentId}",produces = MediaType.APPLICATION_JSON_VALUE)
+public  ResponseEntity<UserDTO> getSelectedStudent(@PathVariable String studentId){
+    System.out.println("student id is"+studentId);
+    var userDTO = new UserDTO(
+            userId: "U001",
+            firstName: "Heshan",
+            lastName:"Perera",
+            addressLine1: "No 123",
+            addressLine2 :"Main Street",
+            addressLine3 :"Colombo 05",
+            city :"Colombo",
+            password: "1234",
+            Role.STUDENT
+    );
+    if(studentId.equals(userDTO.getUserId())){
+        return new ResponseEntity<>(userDTO,OK);
+    }
+
+}
+
+
 }
