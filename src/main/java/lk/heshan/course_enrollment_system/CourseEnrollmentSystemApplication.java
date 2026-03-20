e8a1cbd7555494879e6b84080923d0c3d9f50ad4
heshan <heshansandakelum2018@gmail.com>
2026-03-20 19:11:02 +0530

    start the project

---
+++ src/main/java/lk/heshan/course_enrollment_system/CourseEnrollmentSystemApplication.java
@@ -0,0 +1,13 @@
+package lk.heshan.course_enrollment_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseEnrollmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseEnrollmentSystemApplication.class, args);
+	}
+
+}
